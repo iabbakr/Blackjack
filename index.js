38360bb7b5087bf89244bc15b08e43b6bd541e30
iabbakr <abubakaribrahim603@gmail.com>
2022-12-09 10:47:23 +0100

    added new files

---
+++ index.js
@@ -0,0 +1,18 @@
+let firstCard = 2;
+let secondCard = 4;
+
+let sum = firstCard + secondCard;
+
+let isAlive = true;
+let hasBlackJack = false;
+
+if (sum <= 20){
+    console.log("Do you want to draw a new card?");
+    
+}else if (sum === 21){
+    console.log("Yes! ");
+    hasBlackJack = true;
+}else {
+    console.log("You are out of the game");
+    isAlive = false;
+}
