f46b54f7cde6e0509b94675e51be14af3c782c1c
iabbakr <abubakaribrahim603@gmail.com>
2022-12-17 08:33:30 +0100

    update index.js

--- index.js
+++ index.js
@@ -5,11 +5,11 @@ let sumEl = document.getElementById("sum-el");
 
 let firstCard = 2;
 let secondCard = 4;
-cardEl.textContent = firstCard;
-cardEl.textContent = secondCard;
+cardEl.textContent += firstCard;
+cardEl.textContent += secondCard;
 
 let sum = firstCard + secondCard;
-sumEl.textContent = sum;
+sumEl.textContent += sum;
 
 let isAlive = true;
 let hasBlackJack = false;
@@ -19,16 +19,16 @@ let message = "";
 function start(){
     if (sum <= 20){
         message = "Do you want to draw a new card?";
-        messageEl.textContent = message;
+        messageEl.textContent += message;
         
     }else if (sum === 21){
         message = "Yes! ";
         hasBlackJack = true;
-        messageEl.textContent = message;
+        messageEl.textContent += message;
     }else {
         message = "You are out of the game";
         isAlive = false;
-        messageEl.textContent = message;
+        messageEl.textContent += message;
     };
 
 };
